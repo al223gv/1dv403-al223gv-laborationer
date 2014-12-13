2c33ede3e4ecd2deb89d70815e7e367de23ec122
Alex Lindström <al223gv@student.lnu.se>
2014-12-13 05:52:05 +0000

    bugg-fix, working properly now.

--- 3-gameon/javascript.js
+++ 3-gameon/javascript.js
@@ -43,85 +43,86 @@ var Memory = {
     myfunc: function(evt) {
         evt.preventDefault();
         var img = evt.currentTarget.childNodes[0];
+        if (Memory.firstImageId === null || Memory.secondImageId === null) {
+            if (Memory.uniqueOne === null) {
 
-        if (Memory.uniqueOne === null) {
+                img.setAttribute("src", "memory/pics/" + img.className);
+                Memory.uniqueOne = img.id;
+                Memory.firstImageId = img.className;
 
-            img.setAttribute("src", "memory/pics/" + img.className);
-            Memory.uniqueOne = img.id;
-            Memory.firstImageId = img.className;
-
-            document.getElementById("button" + img.id).removeEventListener('click', Memory.myfunc, false);
-            return;
-        }
-        else if (Memory.uniqueTwo === null && Memory.uniqueOne !== img.id) {
-            document.getElementById("button" + img.id).removeEventListener('click', Memory.myfunc, false);
-            img.setAttribute("src", "memory/pics/" + img.className);
-            Memory.uniqueTwo = img.id;
-            Memory.secondImageId = img.className;
-        }
-
-        if (Memory.secondImageId !== null && Memory.uniqueOne !== Memory.uniqueTwo && Memory.firstImageId === Memory.secondImageId) {
-
-            Memory.pairs += 1;
-            Memory.tries += 1;
-            if (Memory.pairs === 8) {
-                console.log("grattis!");
-                Memory.firstImageId = null;
-                Memory.secondImageId = null;
-                Memory.uniqueOne = null;
-                Memory.uniqueTwo = null;
-                Memory.Pairs = 0;
-                var gameOver = document.querySelector("#gameOver");
-                var h1 = document.createElement("h1");
-                var p1 = document.createElement("p");
-                var button = document.createElement("button");
-                h1.innerHTML = "Grattis, du vann!";
-                p1.innerHTML = "Antal gissningar: " + Memory.tries;
-                button.innerHTML = "Spela igen?";
-
-                gameOver.classList.remove("hidden");
-
-                gameOver.appendChild(h1);
-                gameOver.appendChild(p1);
-                gameOver.appendChild(button);
-
-                button.addEventListener("click", function() {
-                    var gameBox = document.getElementById("gameBox");
-                    gameOver.innerHTML = "";
-                    Memory.tries = 0;
-                    Memory.pairs = 0;
-                    gameOver.classList.add("hidden");
-                    gameBox.innerHTML = "";
-
-                    Memory.init();
-                }, false);
+                document.getElementById("button" + img.id).removeEventListener('click', Memory.myfunc, false);
+                return;
+            }
+            else if (Memory.uniqueTwo === null && Memory.uniqueOne !== img.id) {
+                document.getElementById("button" + img.id).removeEventListener('click', Memory.myfunc, false);
+                img.setAttribute("src", "memory/pics/" + img.className);
+                Memory.uniqueTwo = img.id;
+                Memory.secondImageId = img.className;
             }
 
-            else {
-                console.log("Grattis! Ett nytt par!");
-                Memory.firstImageId = null;
-                Memory.secondImageId = null;
-                Memory.uniqueOne = null;
-                Memory.uniqueTwo = null;
-            }
-        }
-        else if (Memory.secondImageId !== null && Memory.uniqueOne !== Memory.uniqueTwo && Memory.firstImageId !== Memory.secondImageId) {
+            if (Memory.secondImageId !== null && Memory.uniqueOne !== Memory.uniqueTwo && Memory.firstImageId === Memory.secondImageId) {
 
-            setTimeout(function() {
-
-                document.getElementById(Memory.uniqueOne).src = "memory/pics/0.png.png";
-                document.getElementById(Memory.uniqueTwo).src = "memory/pics/0.png.png";
-
-                document.getElementById("button" + Memory.uniqueTwo).addEventListener("click", Memory.myfunc, false);
-                document.getElementById("button" + Memory.uniqueOne).addEventListener('click', Memory.myfunc, false);
-
-                Memory.firstImageId = null;
-                Memory.secondImageId = null;
-                Memory.uniqueOne = null;
-                Memory.uniqueTwo = null;
+                Memory.pairs += 1;
                 Memory.tries += 1;
+                if (Memory.pairs === 8) {
+                    console.log("grattis!");
+                    Memory.firstImageId = null;
+                    Memory.secondImageId = null;
+                    Memory.uniqueOne = null;
+                    Memory.uniqueTwo = null;
+                    Memory.Pairs = 0;
+                    var gameOver = document.querySelector("#gameOver");
+                    var h1 = document.createElement("h1");
+                    var p1 = document.createElement("p");
+                    var button = document.createElement("button");
+                    h1.innerHTML = "Grattis, du vann!";
+                    p1.innerHTML = "Antal gissningar: " + Memory.tries;
+                    button.innerHTML = "Spela igen?";
 
-            }, 1000);
+                    gameOver.classList.remove("hidden");
+
+                    gameOver.appendChild(h1);
+                    gameOver.appendChild(p1);
+                    gameOver.appendChild(button);
+
+                    button.addEventListener("click", function() {
+                        var gameBox = document.getElementById("gameBox");
+                        gameOver.innerHTML = "";
+                        Memory.tries = 0;
+                        Memory.pairs = 0;
+                        gameOver.classList.add("hidden");
+                        gameBox.innerHTML = "";
+
+                        Memory.init();
+                    }, false);
+                }
+
+                else {
+                    console.log("Grattis! Ett nytt par!");
+                    Memory.firstImageId = null;
+                    Memory.secondImageId = null;
+                    Memory.uniqueOne = null;
+                    Memory.uniqueTwo = null;
+                }
+            }
+            else if (Memory.secondImageId !== null && Memory.uniqueOne !== Memory.uniqueTwo && Memory.firstImageId !== Memory.secondImageId) {
+
+                setTimeout(function() {
+
+                    document.getElementById(Memory.uniqueOne).src = "memory/pics/0.png.png";
+                    document.getElementById(Memory.uniqueTwo).src = "memory/pics/0.png.png";
+
+                    document.getElementById("button" + Memory.uniqueTwo).addEventListener("click", Memory.myfunc, false);
+                    document.getElementById("button" + Memory.uniqueOne).addEventListener('click', Memory.myfunc, false);
+
+                    Memory.firstImageId = null;
+                    Memory.secondImageId = null;
+                    Memory.uniqueOne = null;
+                    Memory.uniqueTwo = null;
+                    Memory.tries += 1;
+
+                }, 1000);
+            }
         }
     }
 };
